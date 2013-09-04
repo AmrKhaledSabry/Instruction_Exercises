a3dc7e04d65dd9e01b6cd531a9fafc5b5792f962
jpham247 <jonpham@quriouslyenough.com>
2013-09-03 20:31:12 -0700

    Lynda Java ET - 20130903 Update

--- Java - Lynda Essentials/Exercise Files/05_SyntaxAndFlow/Methods/src/Main.java
+++ Java - Lynda Essentials/Exercise Files/05_SyntaxAndFlow/Methods/src/Main.java
@@ -2,7 +2,21 @@
 public class Main {
 
 	public static void main(String[] args) {
+		doSomething();	
+		
+		loopMe(10);
+	}
 
+
+	private static void loopMe(int top) {
+		for (int i = 0; i < top; i++) {
+			System.out.println("the value is "+i);
+		}
+	}
+	
+	
+	private static void doSomething() {
+		System.out.println("This is the doSomething Method.");
 	}
 
 }
--- Java - Lynda Essentials/Exercise Files/05_SyntaxAndFlow/MethodsWithArgs/src/Calculator.java
+++ Java - Lynda Essentials/Exercise Files/05_SyntaxAndFlow/MethodsWithArgs/src/Calculator.java
@@ -6,11 +6,27 @@ public class Calculator {
 		String s1 = getInput("Enter a numeric value: ");
 		String s2 = getInput("Enter a numeric value: ");
 
+		double result = addTwoValues(s1, s2);
+		
+		System.out.println("The answer is " + result);
+		
+		double resultOfMultiple = addMultipleValues(1,2,3,4,5);
+		System.out.println("The answer of the resultOfMultiple function is " + resultOfMultiple);
+	}
+
+	private static double addTwoValues(String s1, String s2) {
 		double d1 = Double.parseDouble(s1);
 		double d2 = Double.parseDouble(s2);
 		double result = d1 + d2;
-		
-		System.out.println("The answer is " + result);
+		return result;
+	}
+	
+	private static double addMultipleValues(double ... values) {
+		double result = 0d;
+		for (double d : values) {
+			result += d;
+		}
+		return  result;
 	}
 
 	private static String getInput(String prompt) {
